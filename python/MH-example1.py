2144abc2ff228f5a637a3417fbde405eff3b2013
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com.>
2024-05-26 04:19:44 +0530

    added 1st example in MH algo

--- python/MH-example1.py
+++ python/MH-example1.py
@@ -1,11 +1,10 @@
-import numpy as np
-def MetropolisSample(pdf, init_state, step_size, sample_size=SAMPLESIZE):
+def MetropolisSample(targer_function, init_state, step_size, sample_size=SAMPLESIZE):
     current_state = init_state
     samples = []
     for _ in range(sample_size):
-        candidate_state = np.random.normal(loc=current_state, scale=step_size)  # jumping probabitlity
-        accept_prob = np.minimum(1.0, pdf(candidate_state) / pdf(current_state))  # acceptance probability
-        if np.random.uniform() < accept_prob:
+        candidate_state = numpy.random.normal(loc=current_state, scale=step_size)  # jumping probability
+        accept_prob = numpy.minimum(1.0, targer_function(candidate_state) / targer_function(current_state))  # acceptance probability
+        if numpy.random.uniform() < accept_prob:
             current_state = candidate_state
         samples.append(current_state)
-    return np.array(samples)
+    return numpy.array(samples)
